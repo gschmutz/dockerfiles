3130cbc586e2f8a9742a70341007cc1b0f3ff7cf
Guido Schmutz <guido.schmutz@hotmail.com>
2016-06-12 16:15:33 +0200

    added parameter

--- oracle-stream-analytics/samples/1221-domain/container-scripts/configure-silent.sh
+++ oracle-stream-analytics/samples/1221-domain/container-scripts/configure-silent.sh
@@ -3,4 +3,8 @@ sed -i -e "s/%password%/$OSA_PASSWORD/g" /u01/oracle/silent.xml
 sed -i -e "s/%serverName%/$OSA_SERVER_NAME/g" /u01/oracle/silent.xml
 sed -i -e "s/%domainName%/$OSA_DOMAIN_NAME/g" /u01/oracle/silent.xml
 sed -i -e "s/%netioPort%/$OSA_PORT/g" /u01/oracle/silent.xml
+sed -i -e "s/%keystorePassword%/$KEYSTORE_PASSWORD/g" /u01/oracle/silent.xml
+sed -i -e "s/%privatekeyPassword%/$PRIVATEKEY_PASSWORD/g" /u01/oracle/silent.xml
+sed -i -e "s/%dbUsername%/$DB_USERNAME/g" /u01/oracle/silent.xml
+sed -i -e "s/%dbPassword%/$DB_PASSWORD/g" /u01/oracle/silent.xml
 sed -i -e "s!%domainLocation%!$OSA_DOMAIN_HOME!g" /u01/oracle/silent.xml
